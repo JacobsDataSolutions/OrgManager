47fb3efeb28bf6c799d4aba9b42eb6932894efaf
JacobsDataSolutions <jacobsdatasolutions@gmail.com>
2021-02-15 19:07:45 -0600

    Rescaffolded the Identity pages.

---
+++ JDS.OrgManager/JDS.OrgManager.Presentation.WebApi/Areas/Identity/Pages/Account/RegistrationUserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JDS.OrgManager.Presentation.WebApi.Areas.Identity.Pages.Account
+{
+    public enum RegistrationUserType
+    {
+        Employee,
+
+        Employer
+    }
+}
